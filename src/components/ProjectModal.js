4a0afc6048f88df35d9d93d3cd3d64db7add33d7
MariaesSegura <msegber@gmail.com>
2020-09-14 19:20:16 +0200

    Create projects modals

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,31 +1,72 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../stylesheets/Projects.scss';
+import '../stylesheets/ProjectModal.scss';
 
 const ProjectModal = (props) => {
-  //PREVENT DEFAULT
-  const handleModalContentClick = (ev) => {
-    ev.preventDefault();
-  };
-
   return (
-    <Link to="/" className="modal__link">
-      <div className="modal">
-        <div className="modal__dialog">
-          <div className="modal__content" onClick={handleModalContentClick}>
-            <header className="modal__header">
-              <h2 className="modal__title">{props.project.name}</h2>
-              <Link to="/">
-                <span className="modal__close icon fas fa-times"></span>
-              </Link>
-            </header>
-            <section>
-              <img className="card__img" src={props.image} alt="" />
-            </section>
-          </div>
+    <div className="modal">
+      <div className="modal__dialog">
+        <div className="modal__content">
+          <header className="modal__header">
+            <h2 className="modal__title">{props.project.name}</h2>
+            <Link to="/">
+              <i class="modal__close far fa-window-close"></i>
+            </Link>
+          </header>
+          <section className="modal__description">
+            <p className="modal__description-content">
+              {props.project.description}
+            </p>
+          </section>
+
+          <section className="modal__image">
+            <div className="modal__image-responsive desktop">
+              <img
+                className="modal__image-content"
+                src={props.project.desktop}
+                alt={props.project.name}
+              />
+            </div>
+
+            <div className="modal__image-responsive">
+              <img
+                className="modal__image-content"
+                src={props.project.tablet}
+                alt={props.project.name}
+              />
+            </div>
+
+            <div className="modal__image-responsive">
+              <img
+                className="modal__image-content"
+                src={props.project.mobile}
+                alt={props.project.name}
+              />
+            </div>
+          </section>
+          <section className="modal__links">
+            <a
+              className="modal__links-content"
+              href={props.project.github}
+              target="_blank"
+              title="Ir al repositorio del proyecto"
+              rel="noopener noreferrer"
+            >
+              <i class="fab fa-github"> </i> Enlace al repositorio
+            </a>
+            <a
+              className="modal__links-content"
+              href={props.project.githubpages}
+              target="_blank"
+              title="Ir al repositorio del proyecto"
+              rel="noopener noreferrer"
+            >
+              <i class="fas fa-link"></i> Ir a la web
+            </a>
+          </section>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
